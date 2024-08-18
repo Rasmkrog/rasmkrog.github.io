ec125aa9b758343f34ec4aaea26bba777dd03244
Rasmkrog <Rasmkrog@gmail.com>
2024-08-18 20:43:52 +0200

    Added template for css and js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
