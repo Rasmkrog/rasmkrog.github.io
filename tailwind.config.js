0107dbbc4c3db7425c1817862ec65c57aa328e4c
Rasmkrog <Rasmkrog@gmail.com>
2024-08-19 22:18:46 +0200

    added tailwind css scrollbar plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')],
 }
 
