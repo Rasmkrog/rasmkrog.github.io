2d851ef1f52453ab97e93db7fb4c46bb749e2c49
Rasmkrog <Rasmkrog@gmail.com>
2024-08-23 23:00:14 +0200

    added custom navbar to project with screensize adjusting

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        
-        window.scrollTo({
-            top: targetSection.offsetTop,
-            behavior: 'smooth'
-        });
-    });
+// Toggle Mobile Menu
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
 });
