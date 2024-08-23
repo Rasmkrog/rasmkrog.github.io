d88620433357a3427953e61e2632ee4d2ccc0787
Rasmkrog <Rasmkrog@gmail.com>
2024-08-23 22:59:19 +0200

    added custom colors to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'Lightgray': '#638387',
+      'DarkGray': '#222222',
+      'DarkRed': '#721605',
+      'BrightRed': '#AE1C0C',
+      'Orange': '#DE7813',
+      'Yello': '#FCD55B',
+      
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+      amber: colors.amber,
+      },
     extend: {},
   },
   plugins: [require('tailwind-scrollbar')],
