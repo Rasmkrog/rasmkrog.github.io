28cc4d8cd4929cb31c80f34f833d753d29df1bae
Rasmkrog <Rasmkrog@gmail.com>
2024-08-24 23:23:08 +0200

    made smooth transition between landing and next section

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,21 @@ module.exports = {
         amber: colors.amber,
         teal: colors.teal,
         
+
+
       },
+      backgroundImage: theme => ({
+        'smooth-transition': 'linear-gradient(180deg, rgba(26, 15, 11, 0)'+
+        '60%, rgba(26, 15, 11, 0.5) 70%, rgba(31, 65, 71, 0.8) 85%, #21545B 100%)',
+
+        'teal-dark-gradient': 'linear-gradient(180deg, #21545B, #0B2A2D)',
+        
+        'amber-darkteal-gradient': 'linear-gradient(180deg, #E27D01, #21545B)',
+        
+        'teal-amber-diagonal': 'linear-gradient(135deg, #20AAAD, #E27D01)',
+        
+        'darkteal-black-gradient': 'linear-gradient(180deg, #21545B, #1A0F0B)',
+      }),
     },
   },
   plugins: [require('tailwind-scrollbar')],
